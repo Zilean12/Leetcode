3393bdcca8fd5714f34e9a420b04a5db343a5bef
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2024-08-07 23:19:57 +0530

    LeetCode practice - Day 3

---
+++ String Matching in an Array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def stringMatching(self, words):
+        res = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i != j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
+
+sol = Solution()
+words = ["mass","as","hero","superhero"]
+print(sol.stringMatching(words))
+words = ["leetcode","et","code"]
+print(sol.stringMatching(words))
